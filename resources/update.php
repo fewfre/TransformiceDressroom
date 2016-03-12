a0ca40c3961615b425689b1d88cae0f71712f7d6
fewfre <few@dr.com>
2016-03-12 10:06:40 -0500

    Update script now redirects

--- resources/update.php
+++ resources/update.php
@@ -2,5 +2,6 @@
 file_put_contents("x_meli_costumes.swf", fopen("http://www.transformice.com/images/x_bibliotheques/x_meli_costumes.swf", 'r'));
 file_put_contents("x_fourrures.swf", fopen("http://www.transformice.com/images/x_bibliotheques/x_fourrures.swf", 'r'));
 file_put_contents("x_fourrures2.swf", fopen("http://www.transformice.com/images/x_bibliotheques/x_fourrures2.swf", 'r'));
-echo "Update Successful";
+echo "Update Successful! Redirecting...";
+echo '<script>window.setTimeout(function(){ window.location = "../dressroom.swf"; },1000);</script>'
 ?>
