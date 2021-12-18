9716f11779727dac52d342a8fcd66abffaa55b89
fewfre <wesitup@gmail.com>
2021-12-18 12:21:20 -0500

    update script includes new files

--- resources/update.php
+++ resources/update.php
@@ -1,28 +1,31 @@
 <?php
-$resources_base = array("x_meli_costumes", "x_fourrures");
-
 $resources = array();
+$external = array();
+
+$resources_base = array("x_meli_costumes", "costume", "x_fourrures");
 foreach ($resources_base as $filebase) {
 	for ($i = 1; $i <= 6; $i++) {
-		$filename = $i==1 ? "{$filebase}.swf" : "{$filebase}{$i}.swf";
+		$filename = $i==1 && $filebase != "costume" ? "{$filebase}.swf" : "{$filebase}{$i}.swf";
 		$url = "http://www.transformice.com/images/x_bibliotheques/$filename";
-		$code = checkExternalFile($url);
-		if($code == 200 || $code == 300) {
+		if(checkExternalFileExists($url)) {
 			file_put_contents($filename, fopen($url, 'r'));
 			$resources[] = $filename;
+			$external[] = $url;
 		}
 	}
 }
+$external[] = "https://projects.fewfre.com/a801/transformice/dressroom/resources/poses.swf";
 
 $json = json_decode(file_get_contents("config.json"), true);
 $json["packs"]["items"] = $resources;
+$json["packs_external"] = $external;
 $json["cachebreaker"] = time();//md5(time(), true);
 file_put_contents("config.json", json_encode($json));//, JSON_PRETTY_PRINT
 
 echo "Update Successful! Redirecting...";
 echo '<script>window.setTimeout(function(){ window.location = "../"; },1000);</script>';
 
-function checkExternalFile($url)
+function checkExternalFileExists($url)
 {
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_NOBODY, true);
@@ -30,6 +33,6 @@ function checkExternalFile($url)
 	$retCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 	curl_close($ch);
 
-	return $retCode;
+	return $retCode == 200 || $retCode == 300;
 }
 ?>
