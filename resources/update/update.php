7449a041a541f9ed19d337d063bbcb7091445966
fewfre <wesitup@gmail.com>
2024-11-10 15:49:38 -0500

    updated update script

--- resources/update/update.php
+++ resources/update/update.php
@@ -1,130 +1,177 @@
 <?php
 require_once 'utils.php';
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-ini_set('max_execution_time', 3*60);
-set_time_limit(3*60);
+define('URL_TO_CHECK_IF_SCRIPT_HAS_ACCESS_TO_ASSETS', "http://www.transformice.com/images/x_bibliotheques/x_meli_costumes.swf");
 
 setProgress('starting');
 
-// file_put_contents("testing.json", json_encode(array()));
-// function ADD_LOG($msg) {
-// 	$json = json_decode(file_get_contents("testing.json"), true);
-// 	$json[] = $msg;
-// 	file_put_contents("testing.json", json_encode($json, JSON_PRETTY_PRINT));
-// }
-// // Ping to confirm if server is booting us
-// function ping($host, $port, $timeout) { $tB = microtime(true); $fP = fSockOpen($host, $port, $errno, $errstr, $timeout); if (!$fP) { return "down"; } $tA = microtime(true); return round((($tA - $tB) * 1000), 0)." ms"; }
-// ADD_LOG( "PING: " . ping('www.transformice.com', 80, 100) );
-
 // Check if Atelier801 server can be accessed
-$isA801ServerOnline = fetchUrlMetaData("http://www.transformice.com/images/x_bibliotheques/x_meli_costumes.swf");
+$isA801ServerOnline = fetchHeadersOnly(URL_TO_CHECK_IF_SCRIPT_HAS_ACCESS_TO_ASSETS);
 if(!$isA801ServerOnline['exists']) {
 	setProgress('error', [ 'message' => "Update script cannot currently access the Atelier 801 servers - it may either be down, or script might be blocked/timed out" ]);
 	exit;
 }
 
-$resources = array();
-$external = array();
+////////////////////////////////////
+// Core Logic
+////////////////////////////////////
 
-// Normal resources
-$resources_base = array("x_meli_costumes", "costume", "x_fourrures");
-foreach ($resources_base as $filebase) {
-	for ($i = 1; $i <= 8; $i++) {
-		setProgress('updating', [ 'message'=>"Resource: $filebase", 'value'=>$i, 'max'=>8 ]);
-		
-		$filename = $i==1 && $filebase != "costume" ? "{$filebase}.swf" : "{$filebase}{$i}.swf";
-		$url = "http://www.transformice.com/images/x_bibliotheques/$filename";
-		$file = "../$filename";
-		downloadFileIfNewer($url, $file);
-		
-		// Check local file so that if there's a load issue the update script still uses the current saved version
-		if(file_exists($file)) {
-			$resources[] = $filename;
-			$external[] = $url;
-		}
-	}
-}
+// Basic Resources
 
-// Individual resources
-$breakCount = 0; // quit early if enough 404s in a row
-$start = 218; $max = 600;
-for ($i = $start; $i <= $max; $i++) {
-	setProgress('updating', [ 'message'=>'Fur Files', 'value'=>$i-$start+1, 'max'=>$max-$start ]);
-	
-	$filename = "f{$i}.swf";
-	$url = "http://www.transformice.com/images/x_bibliotheques/fourrures/$filename";
-	$filenameLocal = "furs/$filename";
-	$file = "../$filenameLocal";
-	downloadFileIfNewer($url, $file);
-	
-	// Check local file so that if there's a load issue the update script still uses the current saved version
-	if(file_exists($file)) {
-		$resources[] = $filenameLocal;
-		$external[] = $url;
-		$breakCount = 0;
-	} else {
-		$breakCount++;
-		if($breakCount > 5) { break; }
-	}
-}
+list($resourcesBasic, $externalBasic) = updateBasicResources();
+list($resourcesSingles, $externalSingles) = updateFurSingleItemFiles();
 
-//
-// Emoji Loading
-//
-$emojis = array();
-// [prefix, pad]
-$emojiPrefixes = array(
-	["", 0],
-	["1", 2], ["2", 2], ["3", 2], ["4", 2],
-	["1", 4], ["2", 4], ["3", 4]
-);
-foreach ($emojiPrefixes as $prefixData) {
-	list($prefix, $pad) = $prefixData;
-	$start = 0;
-	$max = $start + pow(10, $pad ?: 2); // ?: 2 is for the default "0" case
-	$breakCount = 0; // quit early if enough 404s in a row
-	
-	for ($i = $start; $i <= $max; $i++) {
-		$id = $prefix.str_pad($i, $pad, "0", STR_PAD_LEFT);
-		setProgress('updating', [ 'message'=>"Emoji ($prefix:$pad): $id", 'value'=>$i-$start+1, 'max'=>$max-$start ]);
-		$filename = "$id.png";
-		$url = "https://www.transformice.com/images/x_transformice/x_smiley/$filename";
-		$filenameLocal = "emojis/$filename";
-		$file = "../$filenameLocal";
-		downloadFileIfNewer($url, $file);
-	
-		// Check local file so that if there's a load issue the update script still uses the current saved version
-		if(file_exists($file)) {
-			$emojis[] = $filenameLocal;
-			$breakCount = 0;
-		} else {
-			$breakCount++;
-			if($breakCount > 5) { break; }
-		}
-	}
-}
+// Finally include poses - only needed for external as included in a different way for non-external
+$staticExternal = ["https://projects.fewfre.com/a801/transformice/dressroom/resources/poses.swf"];
 
 setProgress('updating');
+$json = getConfigJson();
+$json["packs"]["items"] = array_merge($resourcesBasic, $resourcesSingles);
+$json["packs_external"] = array_merge($externalBasic, $externalSingles, $staticExternal);
+saveConfigJson($json);
 
-// Finally include poses
-$external[] = "https://projects.fewfre.com/a801/transformice/dressroom/resources/poses.swf";
+// Emojis
 
-$json_path = "../config.json";
-$json = json_decode(file_get_contents($json_path), true);
-$json["packs"]["items"] = $resources;
-$json["packs_external"] = $external;
+$emojis = updateEmojis();
+
+setProgress('updating');
+$json = getConfigJson();
 $json["emojis"] = $emojis;
-$json["cachebreaker"] = time();//md5(time(), true);
-file_put_contents($json_path, json_encode($json));//, JSON_PRETTY_PRINT
+saveConfigJson($json);
+
+// Finished
 
 setProgress('completed');
 echo "Update Successful!";
 
 sleep(10);
 setProgress('idle');
-// echo "Update Successful! Redirecting...";
-// echo '<script>window.setTimeout(function(){ window.location = "../"; },1000);</script>';
+
+////////////////////////////////////
+// Update Functions
+////////////////////////////////////
+
+function updateBasicResources() {
+	$resources = array();
+	$external = array();
+
+	// Normal resources
+	$resources_base = array("x_meli_costumes", "costume", "x_fourrures");
+	foreach ($resources_base as $filebase) {
+		for ($i = 1; $i <= 8; $i++) {
+			setProgress('updating', [ 'message'=>"Resource: $filebase", 'value'=>$i, 'max'=>8 ]);
+			
+			$filename = $i==1 && $filebase != "costume" ? "{$filebase}.swf" : "{$filebase}{$i}.swf";
+			$url = "http://www.transformice.com/images/x_bibliotheques/$filename";
+			$file = "../$filename";
+			downloadFileIfNewer($url, $file);
+			
+			// Check local file so that if there's a load issue the update script still uses the current saved version
+			if(file_exists($file)) {
+				$resources[] = $filename;
+				$external[] = $url;
+			}
+		}
+	}
+	
+	return [$resources, $external];
+}
+
+function updateFurSingleItemFiles() {
+	$resources = array();
+	$external = array();
+	
+	function makeDataFromFileIndex_fourrures($i) {
+		$filename = "f{$i}.swf";
+		$filenameLocal = "furs/$filename";
+		return [
+			'filename' => $filename,
+			'filenameLocal' => $filenameLocal,
+			'localFilePath' => "../$filenameLocal",
+			'url' => "http://www.transformice.com/images/x_bibliotheques/fourrures/$filename",
+		];
+	}
+	
+	// Individual resources
+	$start = 218; $max = 600;
+	
+	// Fetch headers and download any files that need downloading
+	$headerCheckUrlDataList = array_map(fn($i) => makeDataFromFileIndex_fourrures($i), range($start, $max));
+	fetchHeadersOnlyMulti_inChunksWithDataList_downloadIfNeeded($headerCheckUrlDataList, "fur");
+		
+	// Check local file before adding to list, so that if there's a load issue the update script still uses the current saved version
+	setProgress('updating', [ 'message'=>"Generating list of all furs" ]);
+	
+	$breakCount = 0; // quit early if enough 404s in a row
+	for ($i = $start; $i <= $max; $i++) {
+		setProgress('updating', [ 'message'=>'Fur Files', 'value'=>$i-$start+1, 'max'=>$max-$start ]);
+		
+		list('url' => $url, 'localFilePath' => $file, 'filenameLocal' => $filenameLocal) = makeDataFromFileIndex_fourrures($i);
+		
+		// Check local file so that if there's a load issue the update script still uses the current saved version
+		if(file_exists($file)) {
+			$resources[] = $filenameLocal;
+			$external[] = $url;
+			$breakCount = 0;
+		} else {
+			$breakCount++;
+			if($breakCount > 5) { break; }
+		}
+	}
+	
+	return [$resources, $external];
+}
+
+function updateEmojis() {
+	$emojis = array();
+	
+	function makeDataFromFileIndex_smiley($prefix, $pad, $i) {
+		$id = $prefix.str_pad($i, $pad, "0", STR_PAD_LEFT);
+		$filename = "$id.png";
+		$filenameLocal = "emojis/$filename";
+		return [
+			'filename' => $filename,
+			'filenameLocal' => $filenameLocal,
+			'localFilePath' => "../$filenameLocal",
+			'url' => "https://www.transformice.com/images/x_transformice/x_smiley/$filename",
+		];
+	}
+	
+	// [prefix, pad]
+	$emojiPrefixes = array(
+		["", 0],
+		["1", 2], ["2", 2], ["3", 2], ["4", 2],
+		["1", 4], ["2", 4], ["3", 4]
+	);
+	foreach ($emojiPrefixes as $prefixData) {
+		list($prefix, $pad) = $prefixData;
+		$typeName = "Emoji ($prefix:$pad)";
+		$start = 0;
+		$max = $start + pow(10, $pad ?: 2); // ?: 2 is for the default "0" case
+		
+		// Fetch headers and download any files that need downloading
+		$headerCheckUrlDataList = array_map(fn($i) => makeDataFromFileIndex_smiley($prefix, $pad, $i), range($start, $max));
+		fetchHeadersOnlyMulti_inChunksWithDataList_downloadIfNeeded($headerCheckUrlDataList, $typeName);
+		
+		// Check local file before adding to list, so that if there's a load issue the update script still uses the current saved version
+		setProgress('updating', [ 'message'=>"Generating list of all $typeName items" ]);
+		
+		$breakCount = 0; // quit early if enough 404s in a row
+		for ($i = $start; $i <= $max; $i++) {
+			$id = $prefix.str_pad($i, $pad, "0", STR_PAD_LEFT);
+			setProgress('updating', [ 'message'=>"$typeName: $id", 'value'=>$i-$start+1, 'max'=>$max-$start ]);
+			
+			list('localFilePath' => $file, 'filenameLocal' => $filenameLocal) = makeDataFromFileIndex_smiley($prefix, $pad, $i);
+		
+			// Check local file so that if there's a load issue the update script still uses the current saved version
+			if(file_exists($file)) {
+				$emojis[] = $filenameLocal;
+				$breakCount = 0;
+			} else {
+				$breakCount++;
+				if($breakCount > 5) { break; }
+			}
+		}
+	}
+	
+	return $emojis;
+}
--- resources/update/utils.php
+++ resources/update/utils.php
@@ -1,58 +1,246 @@
 <?php
-function downloadFileIfNewer($url, $file) {
-	$resp = fetchUrlMetaData($url);
-	if($resp['exists']) {
-		ifUrlIsNewerThenDownloadToFile($url, $resp['lastModified'], $file);
-	}
-}
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-function ifUrlIsNewerThenDownloadToFile($url, $urlLastModified, $file) {
-	if(checkIfUrlLastModifiedNewerThanFile($urlLastModified, $file)) {
-		downloadUrlToFile($url, $file);
-		return true;
-	}
-	return false;
-}
-function downloadUrlToFile($url, $file) { file_put_contents($file, fopen($url, 'r')); }
-function checkIfUrlLastModifiedNewerThanFile($urlLastModified, $file) {
-	$fileTime = getFileLastModifiedDateTime($file);
-	return $fileTime ? $urlLastModified > $fileTime : true; // If file doesn't exist then url is newer
-}
-function getFileLastModifiedDateTime($file) {
-	$timestamp = filemtime($file);
-	return $timestamp ? new \DateTime("@$timestamp") : null;
-}
-function fetchUrlMetaData($url) {
-	$h = fetchHeadersOnly($url);
-	$statusCode = $h ? explode(" ", $h)[1] : 0;
-	return [
-		'exists' => $statusCode == 200 || $statusCode == 300,
-		'statusCode' => $statusCode,
-		'lastModified' => $h && isset($h['Last-Modified']) ? new \DateTime($h['Last-Modified']) : null,
-	];
-}
-// OLD WAY - seems to be blocked for some reason
-// function fetchHeadersOnly($url) {
-// 	usleep(0.01 * 1000000); // hardcode a slight delay to prevent making requests to fast
-// 	$context = stream_context_create([ 'http' => array('method' => 'HEAD') ]); // Fetch only head to make it faster and to be friendly to server
-// 	return get_headers($url, true, $context);
-// }
-function fetchHeadersOnly($url) {
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL, $url);
-	curl_setopt($curl, CURLOPT_FILETIME, true);
-	curl_setopt($curl, CURLOPT_NOBODY, true);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_HEADER, true);
-	$header = curl_exec($curl);
-	$info = curl_getinfo($curl);
-	curl_close($curl);
-	return $header;
-}
+ini_set('max_execution_time', 5*60);
+set_time_limit(5*60);
 
 function setProgress($state, $data = array()) {
 	$data['state'] = $state;
 	$date_utc = new \DateTime("now", new \DateTimeZone("UTC"));
 	$data['timestamp'] = $date_utc->format('Y-m-d\TH:i:s\Z');
 	file_put_contents("progress.json", json_encode($data));
+}
+
+function getConfigJson() { return json_decode(file_get_contents("../config.json"), true); }
+function saveConfigJson($json) {
+	$json["cachebreaker"] = time();//md5(time(), true);
+	file_put_contents("../config.json", json_encode($json)); //, JSON_PRETTY_PRINT
+}
+
+function ADD_LOG($msg) {
+	$filename = "log.json";
+	$json = array();
+	if(is_file($filename)){
+		$json = json_decode(file_get_contents($filename), true);
+	}
+	$json[] = $msg;
+	file_put_contents($filename, json_encode($json, JSON_PRETTY_PRINT));
+}
+function LOG_PING($hostname='www.transformice.com') {
+	function ping($host, $port, $timeout) { $tB = microtime(true); $fP = fSockOpen($host, $port, $errno, $errstr, $timeout); if (!$fP) { return "down"; } $tA = microtime(true); return round((($tA - $tB) * 1000), 0)." ms"; }
+	ADD_LOG( ping($hostname, 80, 100) );
+}
+
+// function runPhpFileAsync($phpFileName, $args="") {
+// 	$host = SCRIPT_HOST;
+// 	$url = "$host/$phpFileName.php$args";
+// 	// ADD_LOG("NEXT FILE: $url");
+	
+// 	// This script doesn't care about what happens in the next script, so this says for anything calling script to stop listening
+// 	ignore_user_abort(true);
+// 	session_write_close();
+// 	// fastcgi_finish_request(); // This function flushes all response data to the client and finishes the request. This allows for time consuming tasks to be performed without leaving the connection to the client open
+	
+// 	// system("php $phpFileName.php &"); -- "Warning: system() has been disabled for security reasons"
+	
+// 	// this will set the minimum time to wait before proceed to the next line to 1 second - https://stackoverflow.com/a/64619098
+// 	// Same reason as above; we don't want this script to continue running; we don't care about any response
+// 	// $ctx = stream_context_create(['http'=> ['timeout' => 1]]);
+// 	// file_get_contents($url, false, $ctx);
+	
+// 	// https://stackoverflow.com/a/64619098
+// 	$curl = curl_init();
+// 	//this will set the minimum time to wait before proceed to the next line to 100 milliseconds
+// 	curl_setopt_array($curl,[CURLOPT_URL=>$url,CURLOPT_TIMEOUT_MS=>100,CURLOPT_RETURNTRANSFER=>TRUE]);
+// 	curl_exec($curl);
+// 	if (curl_errno($curl)) {
+// 		$error_msg = curl_error($curl);
+// 		ADD_LOG("ERROR: $error_msg");
+// 	}
+// 	//this line will be executed after 100 milliseconds
+// 	curl_close ($curl); 
+// 	// ADD_LOG("Closed; next file was: $url");
+// }
+
+/////////////////////////////
+// Curl related helpers
+/////////////////////////////
+function downloadFileIfNewer($url, $file) { downloadFileIfHeadersAreNewer($url, $file, fetchHeadersOnly($url)); }
+function downloadFileIfHeadersAreNewer($url, $file, $fancyHeaders) {
+	if($fancyHeaders['exists'] && $fancyHeaders['lastModified']) {
+		if(checkIfUrlLastModifiedNewerThanFile($fancyHeaders['lastModified'], $file)) {
+			downloadUrlToFile($url, $file);
+			return true;
+		}
+	}
+	return false;
+}
+function checkIfUrlLastModifiedNewerThanFile($urlLastModified, $file) {
+	$fileTime = getFileLastModifiedDateTime($file);
+	return $fileTime ? $urlLastModified > $fileTime : true; // If file doesn't exist then url is newer
+}
+function getFileLastModifiedDateTime($file) {
+	if(!file_exists($file)) return null;
+	$timestamp = filemtime($file);
+	return $timestamp ? new \DateTime("@$timestamp") : null;
+}
+// function downloadUrlToFile($url, $file) { file_put_contents($file, fopen($url, 'r')); }
+function downloadUrlToFile($url, $file) { curlPutFileContents($url, $file); }
+function curlPutFileContents($url, $file) {
+	$fileTemp = "$file.temp";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url); // Url to download
+	$fp = fopen($fileTemp, 'w');
+	curl_setopt($ch, CURLOPT_FILE, $fp); // where to save it
+	curl_exec ($ch);
+	if (curl_errno($ch)) {
+		$error_msg = curl_error($ch);
+		ADD_LOG("[curlPutFileContents] URL: $url -- $error_msg");
+	} else {
+		if(!!filesize($fileTemp)) {
+			rename($fileTemp, $file);
+		} else {
+			ADD_LOG("[curlPutFileContents] URL: $url -- File size was zero!");
+			unlink($fileTemp);
+		}
+	}
+	curl_close ($ch);
+	fclose($fp);
+}
+// Note: Status is at [0] since it has no associated header name
+// HTTP/1.1 200 OK\r\nContent-Length: 1050186\r\nLast-Modified: Thu, 17 Feb 2022 11:00:04 GMT\r\n\r\n
+// becomes = [ [0] => "HTTP/1.1 200 OK", ["Content-Length"] => "1050186", ["Last-Modified"] => Thu, 17 Feb 2022 11:00:04 GMT ]
+function parseHeadersStringIntoAssoc($headersString) {
+	$headerStrings = explode("\r\n", $headersString);
+	$headers = array();
+	foreach ($headerStrings as $str) {
+		if(!$str) continue;
+		$parts = explode(': ', $str, 2);
+		if(count($parts) === 1) {
+			$headers[] = $str;
+		} else {
+			$headers[$parts[0]] = $parts[1];
+		}
+	}
+	return $headers;
+}
+/**
+ * @return array{ exists:bool, statusCode:int, lastModified:DateTime|null, fileSize:int }
+ */
+function makeFancyHeaderAssocFromNormalHeaderAssoc($headersAssoc) {
+	if(!$headersAssoc) return [ 'exists' => false, 'statusCode' => 0, 'lastModified' => null ];
+	$statusStr = $headersAssoc[0] ?? null; // String in format of: "HTTP/1.1 200 OK"
+	$lastModifiedStr = $headersAssoc['Last-Modified'] ?? null;
+	$fileSizeStr = $headersAssoc['Content-Length'] ?? null;
+	
+	$statusCode = $statusStr ? (int)explode(" ", $statusStr)[1] : 0;
+	return [
+		'exists' => $statusCode == 200 || $statusCode == 300,
+		'statusCode' => $statusCode,
+		'lastModified' => $lastModifiedStr ? new \DateTime($lastModifiedStr) : null,
+		'fileSize' => $fileSizeStr ? (int)$fileSizeStr : 0,
+	];
+}
+function fetchHeadersOnly($url) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_FILETIME, true);
+	curl_setopt($ch, CURLOPT_NOBODY, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	$header = curl_exec($ch);
+	// $info = curl_getinfo($ch);
+	if (curl_errno($ch)) {
+		$error_msg = curl_error($ch);
+		ADD_LOG("[fetchHeadersOnly] URL: $url -- $error_msg");
+	}
+	curl_close($ch);
+	return makeFancyHeaderAssocFromNormalHeaderAssoc( parseHeadersStringIntoAssoc($header) );
+}
+function fetchHeadersOnlyMulti($urls) {
+	$mh = curl_multi_init(); // build the multi-curl handle
+	$handles = array();
+	foreach($urls as $url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_FILETIME, true);
+		curl_setopt($ch, CURLOPT_NOBODY, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		$handles[] = $ch;
+		curl_multi_add_handle($mh, $ch); // Add all $ch to the multi handle
+	}
+	
+	// execute all queries simultaneously, and continue when all are complete
+	$running = null;
+	do {
+		curl_multi_exec($mh, $running);
+	} while ($running);
+
+	//close the handles
+	$headersMulti = array();
+	foreach ($handles as $ch) {
+		$response = curl_multi_getcontent($ch);
+		if($response) {
+			$headersMulti[] = makeFancyHeaderAssocFromNormalHeaderAssoc( parseHeadersStringIntoAssoc($response) );
+		} else {
+			$headersMulti[] = null;
+		}
+		curl_multi_remove_handle($mh, $ch);
+	}
+	curl_multi_close($mh);
+	return $headersMulti;
+}
+/**
+ * While not the most efficient, this works by passing in a list off url data in the full range we want to check, even if most at the end are duds.
+ * However, since this checks them in chunks, any extras in the later chunks will be ignored
+ * @param array{ url:string, ... } $listOfUrlsWithData
+ * @return array{ array{ url:string, headers:FancyHeaders, ... } }
+ */
+function fetchHeadersOnlyMulti_inChunksWithDataList($listOfUrlsWithData, $progressTopic, $chunkSize=32, $breakMax=5) {
+	$chunksOfUrlData = array_chunk($listOfUrlsWithData, $chunkSize); $chunksOfUrlDataLength = count($chunksOfUrlData);
+	
+	$dataList = array();
+	$breakCount = 0; // quit early if enough 404s in a row
+	for ($coudI=0; $coudI < $chunksOfUrlDataLength; $coudI++) { 
+		setProgress('updating', [ 'message'=>"Fetching file headers for $progressTopic: [Chunk] $coudI, [Chunk Size] $chunkSize", "value" => $coudI+1, "max" => $chunksOfUrlDataLength ]);
+		$chunkedDataList = $chunksOfUrlData[$coudI];
+		
+		$urls = array_map(fn($data) => $data['url'], $chunkedDataList);
+		$chunkHeadersList = fetchHeadersOnlyMulti($urls);
+		
+		$len = min(count($chunkedDataList), count($chunkHeadersList));
+		for ($i = 0; $i < $len; $i++) {
+			$data = $chunkedDataList[$i];
+			$data['headers'] = $chunkHeadersList[$i];
+			
+			if($chunkHeadersList[$i] && $chunkHeadersList[$i]['exists']) {
+				$dataList[] = $data;
+				$breakCount = 0;
+			} else {
+				$breakCount++;
+				if($breakCount > $breakMax) { break; }
+			}
+		}
+		if($breakCount > $breakMax) { break; }
+	}
+	
+	return $dataList;
+}
+/**
+ * While not the most efficient, this works by passing in a list off url data in the full range we want to check, even if most at the end are duds.
+ * However, since this checks them in chunks, any extras in the later chunks will be ignored
+ * @param array{ url:string, filename:string, localFilePath:string, ... } $listOfUrlsWithData
+ * @return array{ array{ url:string, filename:string, localFilePath:string, headers:FancyHeaders, ... } }
+ */
+function fetchHeadersOnlyMulti_inChunksWithDataList_downloadIfNeeded($listOfUrlsWithData, $progressTopic, $chunkSize=32, $breakMax=5) {
+	$dataList = fetchHeadersOnlyMulti_inChunksWithDataList($listOfUrlsWithData, $progressTopic, $chunkSize, $breakMax);
+	// loop through returned headers and download the missing ones / ones that have been updated
+	foreach ($dataList as $data) {
+		setProgress('updating', [ 'message'=>"Downloading $progressTopic file if needed: {$data['filename']}" ]);
+		downloadFileIfHeadersAreNewer($data['url'], $data['localFilePath'], $data['headers']);
+	}
+	return $dataList;
 }
